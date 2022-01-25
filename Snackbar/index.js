2841ff18ac7fa4b18a38b71813808b6ac51b8a8c
aniketsingh13 <85512784+aniketsingh13@users.noreply.github.com>
2022-01-26 00:44:02 +0530

    update code in js

--- Snackbar/index.js
+++ Snackbar/index.js
@@ -1,13 +1,25 @@
-const BtnD = document.querySelector(".snack_btn");
+const BtnD = document.querySelectorAll(".snack_btn");
 const SnackbarD = document.querySelector(".Base_snackbar");
+const LeadSnackbarD = document.querySelector(".Lead_snackbar")
+const StackedSnackbarD = document.querySelector(".Stacked_snackbar")
 
-
-
-BtnD.addEventListener("click",()=>{
-    SnackbarD.style.display = "block"
-    setTimeout(()=>{
-     SnackbarD.style.display = "none";
-    },3000)
-    
+BtnD.forEach(btn => {
+    btn.addEventListener("click", (e) => {
+        if (e.target.innerText === "Baseline") {
+            SnackbarD.style.display = "block";
+            setTimeout(() => {
+                SnackbarD.style.display = "none";
+            }, 2000)
+        } else if (e.target.innerText === "Leading") {
+            LeadSnackbarD.style.display = "block";
+            setTimeout(() => {
+                LeadSnackbarD.style.display = "none";
+            }, 2000)
+        } else if (e.target.innerText === "Stacked") {
+            StackedSnackbarD.style.display = "block";
+            setTimeout(() => {
+                StackedSnackbarD.style.display = "none";
+            }, 2000)
+        }
+    })
 })
-
